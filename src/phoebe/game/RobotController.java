c0630f16c56133ab4b5376a88a1bae93a8ed0294
Bán Viktor <viktor.ban@gmail.com>
2015-03-15 18:14:11 +0100

    RobotController attributumai letrehozva

--- src/phoebe/game/RobotController.java
+++ src/phoebe/game/RobotController.java
@@ -4,6 +4,14 @@ import phoebe.basic.Vector;
 
 public class RobotController {
 
+	private Robot actualRobot;
+	private Vector inputSpeedVector;
+	private Vector jumpDestination;
+	private boolean willPlaceOil;
+	private boolean willPlaceGlue;
+	private Game game;
+	private Map map;
+	
 	public RobotController(Robot r, Game g, Map m){}
 	public void toggleOil(){}
 	public void toggleGlue(){}
