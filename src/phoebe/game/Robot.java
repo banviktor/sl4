b4157e2b8b63cbc206356afeb52a74c2ff14defb
Vince Gellár <solarian.vg@gmail.com>
2015-03-18 10:00:36 +0100

    Függvénytörzsek

--- src/phoebe/game/Robot.java
+++ src/phoebe/game/Robot.java
@@ -1,5 +1,6 @@
 package phoebe.game;
 
+import phoebe.Log;
 import phoebe.basic.Color;
 import phoebe.basic.Vector;
 
@@ -15,15 +16,115 @@ public class Robot {
 	private boolean speedHalved;
 	private boolean speedModificationDisabled;
 
-	public Robot(Color c, Vector p){}
-	public Oil createOil(){ return null; }
-	public Glue createGlue(){ return null; }
-	public void setSpeedVector(Vector v){}
-	public void jump(){}
-	public Vector getPosition(){ return null; }
+	
+	/**
+	 * a robot konstruktora
+	 * 
+	 * @param c a robot színe
+	 * @param p a robot kezdeti helye
+	 */
+	public Robot(Color c, Vector p){
+		Log.enterFunction(Robot.class, "Robot", c.toString() + p.toString());
+		
+		this.color = c;
+		this.position = p;
+		
+		this.oilNumber = 3;
+		this.oilNumber = 3;
+		
+		this.speedVector = new Vector(0, 0);
+		
+		this.speedHalved = false;
+		this.speedModificationDisabled = false;
+		
+		Log.exitFunction();
+	}
+	
+	/**
+	 * lerak egy olajfoltot
+	 * 
+	 * @return a létrehozott olajfolt referenciájával tér vissza
+	 */
+	public Oil createOil(){
+		Log.enterFunction(Robot.class, "createOil");
+		
+		Oil o = new Oil(position); //olajfolt létrehozása
+		
+		Log.exitFunction("oil"); //TODO Oil.toString() -> Smudge
+		return o;
+	}
+	
+	/**
+	 * lerak egy ragacsfoltot
+	 * 
+	 * @return a létrehozott ragacsfolt referenciájával tér vissza
+	 */
+	public Glue createGlue(){
+		Log.enterFunction(Robot.class, "createGlue");
+		
+		Glue g = new Glue(position); //ragacsfolt létrehozása
+		
+		Log.exitFunction("glue");
+		return g;
+	}
+	
+	/**
+	 * beállítja a sebességvektort
+	 * 
+	 * @param v az új sebességvektor
+	 */
+	public void setSpeedVector(Vector v){
+		Log.enterFunction(Robot.class, setSpeedVector);
+		
+		speedVector = v;
+		
+		Log.exitFunction();
+	}
+	
+	/**
+	 * végrehajtja az ugrást
+	 */
+	public void jump(){
+		Log.enterFunction(Robot.class, "jump");
+		
+		Log.exitFunction();
+	}
+	
+	/**
+	 * visszatér a robot aktuális pozíciójával
+	 * 
+	 * @return a robot helyvektorával tér vissza
+	 */
+	public Vector getPosition(){
+		Log.enterFunction(Robot.class, "getPosition");
+		
+		Log.exitFunction();
+		return position;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public Vector getSpeedVector(){ return null; }
+	
+	/**
+	 *
+	 *
+	 * @return
+	 */
 	public int getOilNumber(){ return 0; }
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getGlueNumber(){ return 0; }
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public double getDistance(){ return 0; }
 
 	public void halveSpeed(){}
