59595906d8efcbebbed85b7e67b1a55af4241500
Annosz <tadamleanyfalu@gmail.com>
2015-03-18 10:40:57 +0100

    GameController elkészült

--- src/phoebe/game/GameController.java
+++ src/phoebe/game/GameController.java
@@ -1,11 +1,58 @@
 package phoebe.game;
 
+import phoebe.Log;
+
+/**
+ * A játék létrehozásáért és tárolásáért felelõs osztály
+ */
 public class GameController {
 	
+	private Game game;
 	private boolean running;
 
-	public void newGame(){}
-	public void gameEnded(){}
-	public boolean isRunning(){ return running; }
+	/**
+	 * Létrehoz egy új játékot ha nincs épp futás alatt egy másik, és elmenti hogy fut
+	 */
+	public void newGame() {
+		// Függvénybe lépéskor kiírjuk az osztály nevét, a függvényt és a paraméterlistát.
+		Log.enterFunction(GameController.class, "newGame");
+		
+		// Ha nem fut épp játék, létrehoz egy újat
+		// Szkeleton megvalósítás miatt meghívja az isRunningot, mert ott teszi fel a kérdést
+		if (!isRunning()) {
+			Map map = new Map("xtx.txt");
+			game = new Game(5, map, this);
+		}
+		
+		//Függvénybõl kilépés kiírása
+		Log.exitFunction();
+	}
+	
+	/**
+	 * Beállítja, hogy nincs futó játék
+	 */
+	public void gameEnded() {
+		// Függvénybe lépéskor kiírjuk az osztály nevét, a függvényt és a paraméterlistát.
+		Log.enterFunction(GameController.class, "gameEnded");
+		
+		
+		//Függvénybõl kilépés kiírása
+		Log.exitFunction();
+	}
+	
+	/**
+	 * Visszaadja hogy van-e éppen futó játék
+	 * @return
+	 */
+	public boolean isRunning() {
+		// Függvénybe lépéskor kiírjuk az osztály nevét, a függvényt és a paraméterlistát.
+		Log.enterFunction(GameController.class, "isRunning");
+		
+///////////////////		
+		
+		//Függvénybõl kilépés kiírása a visszatérési értékkel
+		Log.exitFunction(String.valueOf(running));		
+		return running;
+	}
 	
 }
