89c57d571a43e4324eae261e5e342acf1a9b19ef
Annosz <tadamleanyfalu@gmail.com>
2015-03-18 11:01:48 +0100

    Kérdés feltéve

--- src/phoebe/game/Game.java
+++ src/phoebe/game/Game.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import phoebe.Application;
 import phoebe.Log;
+import phoebe.UserInput;
 
 /**
  * A játékot megvalósító osztály, mely tárolja a robotokat és ütemezi a köröket,
@@ -33,6 +34,7 @@ public class Game {
 		map = m;
 		gameController = gc;
 		
+		//Függvénybõl kilépés kiírása
 		Log.exitFunction();
 	}
 	
@@ -42,10 +44,27 @@ public class Game {
 	 * @return a sorrendben következõ robot
 	 */
 	public Robot getNextRobot(){
-		Log.enterFunction(Game.class, "getNextRobot");
-	//////////////////////	
+		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
+		Log.enterFunction(Game.class, "getNextRobot");	
 		
+		// Megkérdezzük a felhasználót, hogy véget ért-e egy kör
+		if (UserInput.getBoolean("Véget ért egy kör?", false)) {
+			
+			// Ha kör vége van megkérdezzük a felhasználót, hogy játék vége is-e egyben
+			if(UserInput.getBoolean("Véget ért a játék?", false)) {
+			
+				// Ha vége a játéknak, meghívjuk a hozzá tartozó metódust
+				gameEnd();
+				
+			} else {
+				
+				// Ha nem ért véget a játék de új kör van, akkor ezt jelezzük a Mapnek
+				map.nextRound();
+				
+			}
+		}		
 		
+		//Függvénybõl kilépés kiírása a visszatérési értékkel
 		Log.exitFunction(robots.get(actualRobotNumber).toString());
 		return null;
 	}
@@ -54,8 +73,10 @@ public class Game {
 	 * Törli az aktuális robotot
 	 */
 	public void deleteActualRobot(){
+		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
 		Log.enterFunction(Game.class, "deleteActualRobot");
 		
+		//Függvénybõl kilépés kiírása
 		Log.exitFunction();
 	}
 	
@@ -63,12 +84,14 @@ public class Game {
 	 * A játék végetérését megvalósító metódus, kiválasztja a nyertest és leállítja a játékot
 	 */
 	public void gameEnd(){
+		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
 		Log.enterFunction(Game.class, "gameEnd");
 		
 		for (Robot i : robots) {
 			i.getDistance();
 		}
 		
+		//Függvénybõl kilépés kiírása
 		Log.exitFunction();
 	}
 	
@@ -77,8 +100,10 @@ public class Game {
 	 * @return a játékban lévõrobotok
 	 */
 	public List<Robot> getRobots(){
+		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
 		Log.enterFunction(Game.class, "getRobots");
-				
+		
+		//Függvénybõl kilépés kiírása a visszatérési értékkel
 		Log.exitFunction("List<Robots>");
 		return null;
 	}
