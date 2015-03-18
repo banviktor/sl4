31faf440a416b9771d2956314f53070bcb6034df
Annosz <tadamleanyfalu@gmail.com>
2015-03-18 09:24:16 +0100

    Spaces komment

--- src/phoebe/game/Smudge.java
+++ src/phoebe/game/Smudge.java
@@ -9,35 +9,40 @@ public abstract class Smudge {
 	
 	protected Vector position;
 	protected int remainingRounds;
+
 	
-	
-	/* Konstruktor
-	 * @param	p	A folt pozíciója
+	/** 
+	 * Konstruktor
+	 * @param p a folt pozíciója
 	 */
 	public Smudge(Vector p){}
 	
 	
-	/* Metódus annak eldöntésére, hogy a folt egy adott helyen fejt-e ki hatást
-	 * @param	p	a hely, ahol vizsgáljuk a folt hatásoságát
-	 * @return		logikai érték, mely megadja, hogy a vizsgált helyen fejt-e ki hatást a folt
+	/** 
+	 * Metódus annak eldöntésére, hogy a folt egy adott helyen fejt-e ki hatást
+	 * @param p a hely, ahol vizsgáljuk a folt hatásoságát
+	 * @return logikai érték, mely megadja, hogy a vizsgált helyen fejt-e ki hatást a folt
 	 */
 	public boolean isEffectiveAt(Vector p){ return false; }
 	
 	
-	/* A folt öregítését megvalósító metódus
-	 * @return		a folt új kora
+	/** 
+	 * A folt öregítését megvalósító metódus
+	 * @return a folt új kora
 	 */
 	public int makeOlder(){ return 0; }
 	
 	
-	/* A folt pozícióját lekérdezõ metódus
-	 * @return		a folt pozíciója
+	/** 
+	 * A folt pozícióját lekérdezõ metódus
+	 * @return a folt pozíciója
 	 */
 	public Vector getPosition(){ return null; }
 	
 	
-	/* Absztrakt metódus, mely a folt típusától függõ változást hoz létre az átadott robotban
-	 * @param	r	a robot, amin változtatást kell létrehozni
+	/** 
+	 * Absztrakt metódus, mely a folt típusától függõ változást hoz létre az átadott robotban
+	 * @param r a robot, amin változtatást kell létrehozni
 	 */
 	public abstract void action(Robot r);	
 	
