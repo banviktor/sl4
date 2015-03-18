d1974e995f72fe42b74a45b75943688aee5bed7c
Annosz <tadamleanyfalu@gmail.com>
2015-03-18 09:17:02 +0100

    Kommentezett

--- src/phoebe/game/Smudge.java
+++ src/phoebe/game/Smudge.java
@@ -2,15 +2,43 @@ package phoebe.game;
 
 import phoebe.basic.Vector;
 
+/* Az általános foltokat megvalósító absztarkt osztály. Tárolja a folt helyét,
+ * hátralévõ élettartamát, és megvalósítja a foltok általános metódusait
+ */
 public abstract class Smudge {
 	
 	protected Vector position;
 	protected int remainingRounds;
 	
+	
+	/* Konstruktor
+	 * @param	p	A folt pozíciója
+	 */
 	public Smudge(Vector p){}
+	
+	
+	/* Metódus annak eldöntésére, hogy a folt egy adott helyen fejt-e ki hatást
+	 * @param	p	a hely, ahol vizsgáljuk a folt hatásoságát
+	 * @return		logikai érték, mely megadja, hogy a vizsgált helyen fejt-e ki hatást a folt
+	 */
 	public boolean isEffectiveAt(Vector p){ return false; }
+	
+	
+	/* A folt öregítését megvalósító metódus
+	 * @return		a folt új kora
+	 */
 	public int makeOlder(){ return 0; }
+	
+	
+	/* A folt pozícióját lekérdezõ metódus
+	 * @return		a folt pozíciója
+	 */
 	public Vector getPosition(){ return null; }
+	
+	
+	/* Absztrakt metódus, mely a folt típusától függõ változást hoz létre az átadott robotban
+	 * @param	r	a robot, amin változtatást kell létrehozni
+	 */
 	public abstract void action(Robot r);	
 	
 }
