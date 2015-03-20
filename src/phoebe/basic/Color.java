896f2dc3be7e61b98f174e46a0dfeaa48239b8b6
karolyiar <karolyiar@gmail.com>
2015-03-20 22:24:42 +0100

    undo Color

--- src/phoebe/basic/Color.java
+++ src/phoebe/basic/Color.java
@@ -1,12 +1,7 @@
 package phoebe.basic;
 
 public enum Color {
-	RED(1), YELLOW(2), GREEN(3), BLUE(4), PURPLE(5);
-	private int value;
-	
-	private Color(int value) {
-		this.value = value;
-	}
+	RED, YELLOW, GREEN, BLUE, PURPLE;
 	
 	@Override
 	public String toString(){
