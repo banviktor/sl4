07dc2f150e7efc81f4ef2964586c2abf62d8638d
Annosz <tadamleanyfalu@gmail.com>
2015-04-14 12:48:05 +0200

    PayerRobot interface

--- src/phoebe/game/PlayerRobot.java
+++ src/phoebe/game/PlayerRobot.java
@@ -25,8 +25,6 @@ public class PlayerRobot extends Robot {
 	 */
 	public PlayerRobot(Color c, Vector p){
 		super(p);
-		// Függvénybe lépéskor kiírjuk az osztály nevét, a függvényt és a paraméterlistát
-		Log.enterFunction(Robot.class, "Robot", c.toString()+ ", " + p.toString());
 		
 		this.color = c;
 		
@@ -38,24 +36,25 @@ public class PlayerRobot extends Robot {
 		
 		this.speedHalved = false;
 		this.speedModificationDisabled = false;
-		
-		// Metódusból kilépés kiírása
-		Log.exitFunction();
 	}
+	
+	
+	/**
+	 * A metódus eldönti, hogy a 0.45 egységnyi sugarú robot az adott pozíción található-e
+	 * @param p a megadott pozíció
+	 * @return	logikai érték, mely megadja, hogy a robot az adott pozíción van-e
+	 */
+	public boolean isAt(Vector p) {
+		return (position.distance(p) < 0.45);
+	}
+	
 	
 	/**
 	 * Lerak egy olajfoltot
 	 * @return a létrehozott olajfolt referenciájával tér vissza
 	 */
 	public Oil createOil(){
-		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
-		Log.enterFunction(Robot.class, "createOil");
-		
-		// olajfolt létrehozása
 		Oil o = new Oil(position); 
-		
-		// Metódusból kilépés kiírása a visszatérési értékkel
-		Log.exitFunction(o.toString()); 
 		return o;
 	}
 	
@@ -64,14 +63,7 @@ public class PlayerRobot extends Robot {
 	 * @return a létrehozott ragacsfolt referenciájával tér vissza
 	 */
 	public Glue createGlue(){
-		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
-		Log.enterFunction(Robot.class, "createGlue");
-		
-		// ragacsfolt létrehozása
 		Glue g = new Glue(position); 
-		
-		// Metódusból kilépés kiírása a visszatérési értékkel
-		Log.exitFunction(g.toString());
 		return g;
 	}
 	
@@ -80,13 +72,7 @@ public class PlayerRobot extends Robot {
 	 * @param v az új sebességvektor
 	 */
 	public void setSpeedVector(Vector v){
-		// Függvénybe lépéskor kiírjuk az osztály nevét, a függvényt és a paraméterlistát
-		Log.enterFunction(Robot.class, "setSpeedVector", v.toString());
-		
-		speedVector = v;
-		
-		// Metódusból kilépés kiírása a visszatérési értékkel
-		Log.exitFunction();
+		speedVector = v.normalized();
 	}
 	
 	/**
@@ -113,16 +99,31 @@ public class PlayerRobot extends Robot {
 		Log.exitFunction();
 	}
 	
+	
+	/**
+	 * Megfelezi a robot sebességét
+	 */
+	public void halveSpeed(){
+		// Csak abban az esetben felez, ha ez még nem történt meg a fordulóban
+		if ( speedHalved == false ) {
+			speedVector = speedVector.multiply(0.5);
+			speedHalved = true;
+		}
+	}
+	
+	/**
+	 * Letiltja a sebességmódosítást
+	 */
+	public void disableSpeedModification(){
+		speedModificationDisabled = true;
+	}
+	
+	
 	/**
 	 * Visszatér a robot aktuális sebességével
 	 * @return a robot sebességvektorával tér vissza
 	 */
 	public Vector getSpeedVector(){
-		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
-		Log.enterFunction(Robot.class, "getSpeedVector");
-		
-		// Metódusból kilépés kiírása a visszatérési értékkel
-		Log.exitFunction(speedVector);
 		return speedVector;
 	}
 	
@@ -131,20 +132,6 @@ public class PlayerRobot extends Robot {
 	 * @return olajfoltok száma
 	 */
 	public int getOilNumber(){
-		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
-		Log.enterFunction(Robot.class, "getOilNumber");
-		
-		// Megkérdezzük a felhasználót, hogy vab-e még olaj a készletben
-				if (UserInput.getBoolean("Van még olaj?", true)) {
-					// Ha van, akkor a száma már lényegtelen, fix 3-ra állítjuk
-					oilNumber = 3;
-				} else {
-					// Ha nincs, akkor a készletünk 0
-					oilNumber = 0;
-				}
-		
-		// Metódusból kilépés kiírása a visszatérési értékkel
-		Log.exitFunction(oilNumber);
 		return oilNumber;
 	}
 	
@@ -153,20 +140,6 @@ public class PlayerRobot extends Robot {
 	 * @return ragacsfoltok száma
 	 */
 	public int getGlueNumber(){
-		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
-		Log.enterFunction(Robot.class, "getGlueNumber");
-		
-		// Megkérdezzük a felhasználót, hogy vab-e még ragacs a készletben
-		if (UserInput.getBoolean("Van még ragacs?", true)) {
-			// Ha van, akkor a száma már lényegtelen, fix 3-ra állítjuk
-			glueNumber = 3;
-		} else {
-			// Ha nincs, akkor a készletünk 0
-			glueNumber = 0;
-		}
-		
-		// Metódusból kilépés kiírása a visszatérési értékkel
-		Log.exitFunction(glueNumber);
 		return glueNumber;
 	}
 	
@@ -175,58 +148,15 @@ public class PlayerRobot extends Robot {
 	 * @return megtett távolság
 	 */
 	public double getDistance(){
-		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
-		Log.enterFunction(Robot.class, "getDistence");
-		
-		// Metódusból kilépés kiírása a visszatérési értékkel
-		Log.exitFunction(distance);
 		return distance;
 	}
 	
-	/**
-	 * Megfelezi a robot sebességét
-	 */
-	public void halveSpeed(){
-		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
-		Log.enterFunction(Robot.class, "halveSpeed");
-		
-		// Lassítás
-		if ( speedHalved == false ) {
-			speedVector = speedVector.multiply(0.5);
-			speedHalved = true;
-		}
-		
-		// Metódusból kilépés kiírása
-		Log.exitFunction();
-	}
-	
-	/**
-	 * Letiltja a sebességmódosítást
-	 */
-	public void disableSpeedModification(){
-		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
-		Log.enterFunction(Robot.class, "disableSpeedModification");
-		
-		speedModificationDisabled = true;
-		
-		// Metódusból kilépés kiírása
-		Log.exitFunction();
-	}
 
 	/**
 	 * Megadja, hogy módosíthatja-e a felhasználó a a robot sbességét
 	 * @return módosítható-e vagy nem
 	 */
 	public boolean isSpeedModificationDisabled(){
-		// Függvénybe lépéskor kiírjuk az osztály nevét és a függvényt
-		Log.enterFunction(Robot.class, "isSpeedModificationDisabled");
-		
-		// Megkérdezzük a felhasználót, hogy olajfolton állunk-e, ami kikapcsolja a 
-		// sebességmódosítás lehetõségét
-		speedModificationDisabled = UserInput.getBoolean("Olajfolton állunk?", false);
-		
-		// Metódusból kilépés kiírása a visszatérési értékkel
-		Log.exitFunction(Boolean.toString(speedModificationDisabled));
 		return speedModificationDisabled;
 	}
 	
