ecb08a23f083a4db983012c69c6a4512c198f0ce
Viktor Bán <viktor.ban@gmail.com>
2015-04-14 18:23:46 +0200

    Ha rosszak a paraméterek nem megy tovább

--- src/difftool/Application.java
+++ src/difftool/Application.java
@@ -7,34 +7,36 @@ import java.io.IOException;
 public class Application {
 
 	public static void main(String[] args) {
-		if (args.length != 2)
+		if (args.length != 2){
 			System.out.println("HIBA: a paraméterlista nem megfelelõ.");
-		String file1 = args[0];
-		String file2 = args[1];
-		System.out.println("A(z) " + file1 + " fájlt hasonlítom össze a(z) " + file2 + " fájllal.");
-		try	{
-			BufferedReader br1 = new BufferedReader(new FileReader(file1));
-			
-			try {
-				BufferedReader br2 = new BufferedReader(new FileReader(file2));
-				String line1, line2;
-				int lines = 0;
-				while ((line1 = br1.readLine()) != null && (line2 = br2.readLine()) != null) {
-					if ( !line1.equals(line2) ) {
-						System.out.println("A(z) " + file1 + " a következõ sornál nem egyezik: " + lines);
+		}else{
+			String file1 = args[0];
+			String file2 = args[1];
+			System.out.println("A(z) " + file1 + " fájlt hasonlítom össze a(z) " + file2 + " fájllal.");
+			try	{
+				BufferedReader br1 = new BufferedReader(new FileReader(file1));
+				
+				try {
+					BufferedReader br2 = new BufferedReader(new FileReader(file2));
+					String line1, line2;
+					int lines = 0;
+					while ((line1 = br1.readLine()) != null && (line2 = br2.readLine()) != null) {
+						if ( !line1.equals(line2) ) {
+							System.out.println("A(z) " + file1 + " a következõ sornál nem egyezik: " + lines);
+						}
+						++lines;
 					}
-					++lines;
-				}
-				System.out.println("A(z) " + file1 + " tesztje sikeres volt.");
-				br2.close();
+					System.out.println("A(z) " + file1 + " tesztje sikeres volt.");
+					br2.close();
+				} catch (IOException e) {
+					System.out.println("HIBA: " + file2 + " megnyitása sikertelen.");
+				} 
+				
+				br1.close();
 			} catch (IOException e) {
-				System.out.println("HIBA: " + file2 + " megnyitása sikertelen.");
-			} 
-			
-			br1.close();
-		} catch (IOException e) {
-			System.out.println("HIBA: " + file1 + " megnyitása sikertelen.");
-		}
+				System.out.println("HIBA: " + file1 + " megnyitása sikertelen.");
+			}
+		}		
 	}
 
 }
