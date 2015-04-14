c82121807b2eda7770921fd026ee38f4cde9ee3d
banviktor <viktor.ban@gmail.com>
2015-04-14 22:27:47 +0200

    UTF-8 run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,24 +1,24 @@
-::A konzolra való írás kikapcsolása
+::A konzolra valÃ³ Ã­rÃ¡s kikapcsolÃ¡sa
 @echo off
 
-::Könyvtárak
+::KÃ¶nyvtÃ¡rak
 set test_input_dir=tests/input
 set test_output_dir=tests/results
 set test_compare_dir=tests/expected
 
-::A kimeneti fájl meghatározása
+::A kimeneti fÃ¡jl meghatÃ¡rozÃ¡sa
 set output=%1
 if %1.==. set output=test_results.txt
 
-::A kimeneti fájl létrehozása üresen
+::A kimeneti fÃ¡jl lÃ©trehozÃ¡sa Ã¼resen
 type NUL > %output%
 
-::Tesztesetek lefuttatása és a kimeneti fájlhoz fûzése
+::Tesztesetek lefuttatÃ¡sa Ã©s a kimeneti fÃ¡jlhoz fÅ±zÃ©se
 
 ::Teszt 1
 java -cp ./bin phoebe.Application < %test_input_dir%/teszt_2jatek_rossz.txt > %test_output_dir%/teszt_2jatek_rossz.txt
 java -cp ./bin difftool.Application %test_output_dir%/teszt_2jatek_rossz.txt %test_compare_dir%/teszt_2jatek_rossz.txt >> %output%
 
-::Tesztelés vége
+::TesztelÃ©s vÃ©ge
 echo A teszteles veget ert.
 pause
