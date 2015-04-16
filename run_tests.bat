3dea01a688031deb016ca51fa0e7610beb5c4121
banviktor <viktor.ban@gmail.com>
2015-04-16 13:42:46 +0200

    prefixek a batchből is kivéve

--- run_tests.bat
+++ run_tests.bat
@@ -8,7 +8,7 @@ set test_compare_dir=tests/expected
 
 ::A kimeneti fájl meghatározása
 set output=%1
-if %1.==. set output=test_results.txt
+if %1.==. set output=run_tests.log
 
 ::A kimeneti fájl létrehozása üresen
 type NUL > %output%
@@ -16,8 +16,8 @@ type NUL > %output%
 ::Tesztesetek lefuttatása és a kimeneti fájlhoz fűzése
 
 ::Teszt 1
-java -cp ./bin phoebe.Application < %test_input_dir%/teszt_2jatek_rossz.txt > %test_output_dir%/teszt_2jatek_rossz.txt
-java -cp ./bin difftool.Application %test_output_dir%/teszt_2jatek_rossz.txt %test_compare_dir%/teszt_2jatek_rossz.txt >> %output%
+java -cp ./bin phoebe.Application < %test_input_dir%/2jatek_rossz.txt > %test_output_dir%/2jatek_rossz.txt
+java -cp ./bin difftool.Application %test_output_dir%/2jatek_rossz.txt %test_compare_dir%/2jatek_rossz.txt >> %output%
 
 ::Tesztelés vége
 echo A teszteles veget ert.
