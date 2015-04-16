2b508de57641bb12c09d243d7ed8025a13b8afb8
banviktor <viktor.ban@gmail.com>
2015-04-16 13:51:06 +0200

    Megáll ha hiba van, akkor írja hogy jó, ha tényleg az

--- src/difftool/Application.java
+++ src/difftool/Application.java
@@ -20,13 +20,18 @@ public class Application {
 					BufferedReader br2 = new BufferedReader(new FileReader(file2));
 					String line1, line2;
 					int lines = 0;
+					boolean matches = true;
 					while ((line1 = br1.readLine()) != null && (line2 = br2.readLine()) != null) {
 						if ( !line1.equals(line2) && !line1.equals("") && !line2.equals("") ) {
 							System.out.println("A(z) " + file1 + " a következő sornál nem egyezik: " + lines);
+							matches = false;
+							break;
 						}
 						++lines;
 					}
-					System.out.println("A(z) " + file1 + " tesztje sikeres volt.");
+					if(matches){
+						System.out.println("A(z) " + file1 + " tesztje sikeres volt.");
+					}						
 					br2.close();
 				} catch (IOException e) {
 					System.out.println("HIBA: " + file2 + " megnyitása sikertelen.");
