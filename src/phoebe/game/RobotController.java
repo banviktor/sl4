0a42664f0a52b97298525d9ae7baa4bb547af03e
banviktor <viktor.ban@gmail.com>
2015-04-17 13:40:38 +0200

    Szebb összeadás

--- src/phoebe/game/RobotController.java
+++ src/phoebe/game/RobotController.java
@@ -174,12 +174,7 @@ public class RobotController {
 	 * @return az ugrás céljának vektora
 	 */
 	public Vector getJumpDestination(){
-		return new Vector(inputSpeedVector.getX()
-				+actualRobot.getPosition().getX()
-				+actualRobot.getSpeedVector().getX(), 
-				inputSpeedVector.getY()
-				+actualRobot.getPosition().getY()
-				+actualRobot.getSpeedVector().getY());
+		return actualRobot.getPosition().add(actualRobot.getSpeedVector()).add(inputSpeedVector);		
 	}
 	
 }
