744add14d555ce00d6ec10bf03663f9b514133cb
Vince Gellár <solarian.vg@gmail.com>
2015-04-22 11:30:21 +0200

    renamed resource using common naming method

---
+++ src/phoebe/control/GameButtonListener.java
@@ -0,0 +1,23 @@
+package phoebe.control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import phoebe.gfx.Window;
+
+public class GameButtonListener implements ActionListener{
+	
+	private Window win;
+	
+	public GameButtonListener(Window w) {
+		win = w;
+	}
+	
+
+	public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand() == "2 player") {
+        	win.newGame(2);
+        }
+    } 
+	
+}
