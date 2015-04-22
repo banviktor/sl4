73ef3a19f08a460501c2abe9951d5adc9c3b0298
Vince Gellár <solarian.vg@gmail.com>
2015-04-22 10:23:18 +0200

    added gfx package

---
+++ src/phoebe/gfx/Window.java
@@ -0,0 +1,18 @@
+package phoebe.gfx;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+	JButton smudge;
+	JButton oil;
+	JButton jump;
+	
+	public Window(){
+		initComponents();
+	}
+	
+	private void initComponents(){
+		
+	}
+}
