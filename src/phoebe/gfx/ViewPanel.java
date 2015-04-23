83b12bd733cb36f9b9cc53e5a5ed94100dc3bb44
karolyiar <karolyiar@gmail.com>
2015-04-24 01:16:28 +0200

    DrawMap implemented

--- src/phoebe/gfx/ViewPanel.java
+++ src/phoebe/gfx/ViewPanel.java
@@ -1,16 +1,24 @@
 package phoebe.gfx;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
+import java.util.List;
 
 import javax.swing.JPanel;
 
+import phoebe.basic.Line;
+import phoebe.basic.Vector;
+import phoebe.game.GameController;
+import phoebe.game.Map;
+
 public class ViewPanel extends JPanel{
+	private static final long serialVersionUID = -138224603482222475L;
+	private GameController gc;
 	
-	public ViewPanel() {
-		
+	public ViewPanel(GameController gc) {
+		this.gc = gc;
 	}
 	
 	@Override
@@ -21,13 +29,30 @@ public class ViewPanel extends JPanel{
 	
 	private void drawAll(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.RED);
-		g2d.fillOval(0, 0, 30, 30);
-		g2d.drawOval(0, 50, 30, 30);		
-		g2d.fillRect(50, 0, 30, 30);
-		g2d.drawRect(50, 50, 30, 30);
 
-		g2d.draw(new Ellipse2D.Double(0, 100, 30, 30));
+		drawMap(g2d);
 	}
 	
+	private void drawMap(Graphics2D g) {
+		Map map = gc.getMap();
+		int lineWidth = transform(map.getLineWidth());
+		List<Line> lines = map.getLines();
+		g.setColor(Color.GRAY);
+		for(Line l : lines) {
+			Vector v1 = l.getVector1();
+			Vector v2 = l.getVector2();
+			int x1 = transform(v1.getX());
+			int y1 = transform(v1.getY());
+			int x2 = transform(v2.getX());
+			int y2 = transform(v2.getY());
+			g.fillOval(x1-lineWidth/2, y1-lineWidth/2, lineWidth, lineWidth);
+			g.setStroke(new BasicStroke(lineWidth));
+			g.drawLine(x1, y1, x2, y2);
+			g.fillOval(x2-lineWidth/2, y2-lineWidth/2, lineWidth, lineWidth);
+		}
+	}
+	
+	private int transform(double d) {
+		return (int)( d / 10.0 * getWidth() );
+	}
 }
