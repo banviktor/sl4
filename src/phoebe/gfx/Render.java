c30fe707bd39fea4f41c0876ed87a3007949430c
karolyiar <karolyiar@gmail.com>
2015-04-24 16:41:29 +0200

    gameloop implemented

--- src/phoebe/gfx/Render.java
+++ src/phoebe/gfx/Render.java
@@ -1,5 +1,7 @@
 package phoebe.gfx;
 
+import java.awt.image.ImageObserver;
+
 public class Render extends Thread {
 	
 	MainWindow mw;
@@ -11,12 +13,36 @@ public class Render extends Thread {
 
 	@Override
 	public void run() {
-		try {
-			this.sleep(33);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		boolean RENDER_TIME = true;
+		boolean running = true;
+		long initialTime = System.nanoTime();
+	    final int TARGET_FPS = 60;
+	    final double OPTIMAL_TIME = 1000000000 / TARGET_FPS;
+	    double deltaT = 0;
+	    long timer = System.currentTimeMillis();
+	    int frames = 0;
+	    
+		while(true){
+			 long currentTime = System.nanoTime();
+			 deltaT += (currentTime - initialTime) / OPTIMAL_TIME;
+			 initialTime = currentTime;
+			 
+			 if (deltaT >= 1) {
+				 if (mw.vp != null)
+					 // blokkol a repaint alatt, így mérhető a szükséges idő
+					 mw.vp.paintImmediately(0, 0, ImageObserver.WIDTH, ImageObserver.HEIGHT);
+		             frames++;
+		             deltaT--;
+		    }
+		    
+			 if (System.currentTimeMillis() - timer > 1000) {
+		            if (RENDER_TIME) {
+		                System.out.println(String.format("FPS: %s", frames));
+		            }
+		            frames = 0;
+		            timer += 1000;
+		        }
 		}
-		mw.vp.repaint();
 	}
 
 }
