573ce295561e433a256c249a063be80e3afc4fe5
Annosz <tadamleanyfalu@gmail.com>
2015-04-24 22:28:19 +0200

    GameMouseListener

---
+++ src/phoebe/control/GameMouseListener.java
@@ -0,0 +1,60 @@
+package phoebe.control;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import phoebe.game.RobotController;
+
+public class GameMouseListener implements MouseMotionListener, MouseListener{
+
+	private RobotController rc;
+	
+	
+	public GameMouseListener(RobotController rc) {
+		this.rc = rc;
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
