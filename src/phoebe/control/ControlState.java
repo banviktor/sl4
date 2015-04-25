52bdd698d75e6c8736f74e86024c08dda062ffa4
karolyiar <karolyiar@gmail.com>
2015-04-25 20:11:04 +0200

    beauty++

--- src/phoebe/control/ControlState.java
+++ src/phoebe/control/ControlState.java
@@ -2,21 +2,16 @@ package phoebe.control;
 
 public class ControlState {
 	private static boolean enabled = true;
-	private static Object syncObject = new Object();
 	
-	public static void release(){
-		synchronized (syncObject){
-			enabled = true;
-		}
+	public synchronized static void release(){
+		enabled = true;
 	}
 	
-	public static boolean get(){
-		synchronized (syncObject){
-			if(enabled){
-				enabled = false;
-				return true;
-			}
-			return false;
+	public synchronized static boolean get(){
+		if(enabled){
+			enabled = false;
+			return true;
 		}
+		return false;
 	}
 }
