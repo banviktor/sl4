3aafef2f78acbebd43e8b87f5e55f76076d014f5
Vince Gellár <solarian.vg@gmail.com>
2015-04-27 16:52:31 +0200

    robots get real inputvectors

--- src/phoebe/control/GameMouseListener.java
+++ src/phoebe/control/GameMouseListener.java
@@ -4,6 +4,9 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
+import phoebe.basic.Vector;
+import phoebe.game.Map;
+import phoebe.game.PlayerRobot;
 import phoebe.game.RobotController;
 
 public class GameMouseListener extends MouseAdapter implements MouseMotionListener{
@@ -16,8 +19,13 @@ public class GameMouseListener extends MouseAdapter implements MouseMotionListen
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		PlayerRobot actualRobot = rc.getActualRobot();
+		double xCoord = (Map.size*e.getX()/600)-actualRobot.getPosition().getX();
+		double yCoord = (Map.size*e.getY()/600)-actualRobot.getPosition().getY();
+		Vector input = new Vector(xCoord, yCoord);
+		System.out.println(input.getX() + " " + input.getY());
+		rc.setInputSpeedVector(input);
+		System.out.println("input set");
 	}
 
 	@Override
