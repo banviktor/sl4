ac5175c8ea9e073712981c883c6555d05dd43537
karolyiar <karolyiar@gmail.com>
2015-05-07 19:26:10 +0200

    glitch fixed, sleep between renders

--- src/phoebe/gfx/Render.java
+++ src/phoebe/gfx/Render.java
@@ -12,13 +12,13 @@ public class Render extends Thread {
 
 	@Override
 	public void run() {
-		boolean RENDER_TIME = true;
-		long initialTime = System.nanoTime();
-	    final int TARGET_FPS = 60;
+		final boolean RENDER_TIME = true;
+	    final long TARGET_FPS = 60;
 	    final double OPTIMAL_TIME = 1000000000 / TARGET_FPS;
+	    long initialTime = System.nanoTime();
 	    double deltaT = 0;
 	    long timer = System.currentTimeMillis();
-	    int frames = 0;
+	    long frames = 0;
 	    long currentTime;
 	    ViewPanel vp = mw.getViewPanel();
 	    
@@ -27,24 +27,38 @@ public class Render extends Thread {
 			 deltaT += (currentTime - initialTime) / OPTIMAL_TIME;
 			 initialTime = currentTime;
 			 
-			 if (deltaT >= 1) {
+			 // ha sok idő van hátra a következő renderelésig,
+			 // akkor vár 0.9*renderközittidőt
+			 if (deltaT <= 0.1){
+			    try {
+					Thread.sleep((long) (900/TARGET_FPS));
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			 // ha renderelni kell
+			 } else if (deltaT >= 1) {
 				 if (vp != null)
-					 // blokkol a repaint alatt, így mérhető a szükséges idő
-					vp.paintImmediately(0, 0, vp.getHeight(), vp.getWidth());
+					//vp.paintImmediately(vp.getBounds()); // blokkol a repaint alatt, így mérhető a szükséges idő
+					 vp.repaint();
 		         frames++;
 		         deltaT--;
 		    }
-		    
-			 if (System.currentTimeMillis() - timer > 1000) {
-		            if (RENDER_TIME) {
-		                System.out.println(String.format("FPS: %s", frames));
-		            }
-		            frames = 0;
-		            timer += 1000;
-		        }
+			 
+			// számolja a képkockákat, fpst
+			if (RENDER_TIME) {
+				if (System.currentTimeMillis() - timer > 1000) {
+					System.out.println(String.format("FPS: %s", frames));
+					frames = 0;
+					timer += 1000;
+				}
+			}
 		}
 	}
 	
+	/**
+	 * Megállítja a renderelést
+	 */
 	public void stopSignal(){
 		stopSignal = true;
 	}
