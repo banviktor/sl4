d851a4a212076f0babb58b5a5dbb4419cbd915e8
karolyiar <karolyiar@gmail.com>
2015-05-08 13:01:43 +0200

    comments

--- src/phoebe/control/ControlState.java
+++ src/phoebe/control/ControlState.java
@@ -3,10 +3,17 @@ package phoebe.control;
 public class ControlState {
 	private static boolean enabled = true;
 	
+	/**
+	 * Elengedi a vezérlést.
+	 */
 	public synchronized static void release(){
 		enabled = true;
 	}
 	
+	/**
+	 * Lefoglalja a vezérlést. Ha nem sikerült, false-t ad vissza.
+	 * @return sikeres foglalás
+	 */
 	public synchronized static boolean get(){
 		if(enabled){
 			enabled = false;
