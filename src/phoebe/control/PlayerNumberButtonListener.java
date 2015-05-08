14ac73256bc2818bc2b889407ee3ab42f110c587
Vince Gellár <solarian.vg@gmail.com>
2015-05-08 12:49:46 +0200

    comments in PlayerNumberButtonListener

--- src/phoebe/control/PlayerNumberButtonListener.java
+++ src/phoebe/control/PlayerNumberButtonListener.java
@@ -7,14 +7,20 @@ import phoebe.gfx.MainWindow;
 
 public class PlayerNumberButtonListener implements ActionListener{
 	
+	//A MainWindowon keresztül indít új játékot
 	private MainWindow mw;
 	
 	public PlayerNumberButtonListener(MainWindow mw) {
 		this.mw = mw;
 	}
 	
-
+	/**
+	 * Attól függően, hogy melyik gombra kattint a felhasználó,
+	 * a gomb által mutatott számú játékossal indít új játékot
+	 */
 	public void actionPerformed(ActionEvent e) {
+		
+		//A kapott parancs értelmezése
         if(e.getActionCommand().equals("2")) {
         	mw.newGame(2);
         } else if(e.getActionCommand().equals("3")) {
