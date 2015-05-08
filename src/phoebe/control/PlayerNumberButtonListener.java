90194666d7fe2830031f80ad86a6bbd2619dc283
Vince Gellár <solarian.vg@gmail.com>
2015-05-08 13:23:46 +0200

    moar comments

--- src/phoebe/control/PlayerNumberButtonListener.java
+++ src/phoebe/control/PlayerNumberButtonListener.java
@@ -22,12 +22,14 @@ public class PlayerNumberButtonListener implements ActionListener{
 		
 		//A kapott parancs értelmezése
         if(e.getActionCommand().equals("2")) {
+        	//A parancsban átadott gomb számának megegyező játékossal indul el a játék
         	mw.newGame(2);
         } else if(e.getActionCommand().equals("3")) {
         	mw.newGame(3);
         } else if(e.getActionCommand().equals("4")) {
         	mw.newGame(4);
         } else {
+        	//Ha az előzőek közűl egyik sem, akkor az 5ös gombra kattintottak
         	mw.newGame(5);
         }
     } 
