4d951596363707ab0dd1c065bf6014857f1ef059
Vince Gellár <solarian.vg@gmail.com>
2015-05-08 12:23:48 +0200

    comments in GameMouseListener

--- src/phoebe/control/GameMouseListener.java
+++ src/phoebe/control/GameMouseListener.java
@@ -11,7 +11,8 @@ import phoebe.game.PlayerRobot;
 import phoebe.game.RobotController;
 
 public class GameMouseListener extends MouseAdapter implements MouseMotionListener{
-
+	
+	//Ismernie kell a GameControllert, hogy irányítani tudja a játékot
 	private GameController gc;
 		
 	public GameMouseListener(GameController gc) {
@@ -19,31 +20,52 @@ public class GameMouseListener extends MouseAdapter implements MouseMotionListen
 	}
 	
 	@Override
+	/**
+	 * Kattintás esetén beállítja az inputvectort
+	 * @param e a függvényhívást kiváltó esemény
+	 */
 	public void mouseClicked(MouseEvent e) {
 		mouseInput(e);
 	}
 
 	@Override
+	/**
+	 * Az egér húzása esetén állítja be az inputvectort
+	 * @param e
+	 */
 	public void mouseDragged(MouseEvent e) {
 		mouseInput(e);	
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		//Nem használjuk
 	}
 	
+	/**
+	 * A közös inputvectort beállító függvény
+	 * @param e a kiváltó esemény
+	 */
 	private void mouseInput(MouseEvent e){
 		RobotController rc = gc.getRobotController();
 		PlayerRobot actualRobot = rc.getActualRobot();
+		
+		//Amennyiben nincsen robot, nicnsen minek inputvectort adni
 		if(actualRobot == null)
 			return;
+		
+		//Kiszámoljuk a kettintás koordinátájit a játék koordinátarendszerében
 		Vector click = new Vector((Map.size*e.getX()/600), (Map.size*e.getY()/600));
+		
+		//Létrehozzuk az inputvectort
 		Vector input = new Vector(actualRobot.getPosition(), click);
+		
+		//Amennyiben a roboton kattintunk, az input vector 0 hosszú lesz
 		if(click.distance(actualRobot.getPosition()) < actualRobot.getRadius()){
 			input = new Vector(0, 0);
 		}
+		
+		//Átadjuk az inputvectort
 		rc.setInputSpeedVector(input);
 	}
 
